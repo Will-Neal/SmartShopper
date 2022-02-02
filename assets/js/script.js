e6b562c55b0c23ea8b2976566107d0b64f053a01
Will Neal <willwneal@gmail.com>
2022-02-02 11:47:57 -0500

    Got Ebay API call working

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,16 +24,16 @@ function getAmazon(event){
         return response.json();
     })
     .then(function(data) {
-        console.log("data: " + data)
-        console.log("image: " + data.results[0].image)
+        // console.log("data: " + data)
+        // console.log("image: " + data.results[0].image)
         // console.log(data.results[0].name)
-        console.log("price: $" + data.results[0].price)
-        console.log("Ratings: " + data.results[0].stars)
-        console.log("url: " + data.results[0].url)
+        // console.log("price: $" + data.results[0].price)
+        // console.log("Ratings: " + data.results[0].stars)
+        // console.log("url: " + data.results[0].url)
         amazonImg.src = data.results[0].image
         var urlSplit = data.results[0].url.split("/")
         var productId = urlSplit[5]
-        console.log(productId)
+        // console.log(productId)
         fetch("https://wolf-amazon-data-scraper.p.rapidapi.com/products/" + productId + "?api_key=59ef84be287bba26357f5519b0058332", {
 	    "method": "GET",
 	    "headers": {
@@ -56,4 +56,24 @@ function getAmazon(event){
     
 }
 
+function getEbay(event){
+    event.preventDefault();
+    console.log("running getEbay Function")
+    fetch("https://ebay-products-search-scraper.p.rapidapi.com/products?query=camera&page=1&Item_Location=north_america", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "ebay-products-search-scraper.p.rapidapi.com",
+		"x-rapidapi-key": "2fe7923d20msh849834e2d765975p110705jsnd39259dc0189"
+	}
+    })
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data){
+        console.log(data)
+        console.log("Price: " + data.products[0].price)
+    })
+}
+
+searchBtn.addEventListener("click", getEbay)
 searchBtn.addEventListener("click", getAmazon)
