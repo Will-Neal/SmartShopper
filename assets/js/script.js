aebf38883a903afc6824a6d60755afdd4e050d4b
Yaakov Yisrael Benjamin <yyisrael613@gmail.com>
2022-02-03 14:35:43 -0500

    added comments to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,32 @@
 // Initialize Variables
-var searchBtn = document.querySelector("#searchBtn");
-var card = document.getElementsByClassName("card");
-var amazonImg = document.querySelector("#amazonCardImage");
-var amazonDescription = document.querySelector("#amazonCardText");
-var amazonItemName = document.querySelector("#amazonItemName");
-var amazonPrice = document.querySelector("#amazonPrice");
-var amazonRating = document.querySelector("#amazonRating");
-var amazonButton = document.querySelector("#goToAmazon");
-var ebayImg = document.querySelector("#ebayImage");
-var ebayItemName = document.querySelector("#ebayItemName");
-var ebayPrice = document.querySelector("#ebayPrice");
-var ebayRating = document.querySelector("#ebayRating");
-var ebayButton = document.querySelector("#goToEbay");
-var spinner = document.querySelector(".spinner-border");
+// GENERAL
+var searchBtn = document.querySelector("#searchBtn"); // Search Button
+var card = document.getElementsByClassName("card");   // Info Card
+var spinner = document.querySelector(".spinner-border"); // Spinner
+// AMAZON
+var amazonImg = document.querySelector("#amazonCardImage"); // Amazon Card Image
+var amazonItemName = document.querySelector("#amazonItemName"); // Amazon item number
+var amazonPrice = document.querySelector("#amazonPrice"); // Amazon Price
+var amazonRating = document.querySelector("#amazonRating"); // Amazon Rating
+var amazonDescription = document.querySelector("#amazonCardText"); // Amazon Description
+var amazonButton = document.querySelector("#goToAmazon"); // Buy on Amazon
+// EBAY
+var ebayImg = document.querySelector("#ebayCardImage"); // eBay Card Image
+var ebayItemName = document.querySelector("#ebayItemName"); // eBay item number
+var ebayPrice = document.querySelector("#ebayPrice"); // eBay Price
+var ebayRating = document.querySelector("#ebayRating"); // eBay Rating
+var ebayButton = document.querySelector("#goToEbay"); // Buy on eBay
 
-// Call Amazon API
+// Amazon API Function
 function getAmazon(event){
-    event.preventDefault() // Prevent Default
-    card[0].style.display = "none"; // Hide card
+    event.preventDefault()            // Prevent Default
+    card[0].style.display = "none";   // Hide card
     spinner.style.display = "inline"; // Show spinner
-    var userInput = document.querySelector("#userInput").value
-    var searchURL = "https://wolf-amazon-data-scraper.p.rapidapi.com/search/"+ userInput +"?api_key=59ef84be287bba26357f5519b0058332"
-    fetch(searchURL, {
+
+    var userInput = document.querySelector("#userInput").value; // Get User Input
+    var searchURL = "https://wolf-amazon-data-scraper.p.rapidapi.com/search/"+ userInput +"?api_key=59ef84be287bba26357f5519b0058332"; // URL
+
+    fetch(searchURL, { // Call the Amazon API
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "wolf-amazon-data-scraper.p.rapidapi.com",
@@ -32,19 +37,23 @@ function getAmazon(event){
         return response.json();
     })
     .then(function(data) {
-        amazonImg.src = data.results[0].image
-        amazonPrice.textContent = "$" + data.results[0].price
-        if (data.results[0].stars === undefined) {
-            amazonRating.textContent = "No reviews yet ⭐"
-        } else {
-            amazonRating.textContent = data.results[0].stars + " ⭐"
+        amazonImg.src = data.results[0].image;                 // Amazon Image
+        amazonPrice.textContent = "$" + data.results[0].price; // Amazon Price
+
+        // REVIEWS
+        if (data.results[0].stars === undefined) {          // if no reviews...
+            amazonRating.textContent = "No reviews yet ⭐"; // print 'no reviews'
+        } else {                                                      // otherwise...
+            amazonRating.textContent = data.results[0].stars + " ⭐"; // display reviews
         }
-        amazonButton.addEventListener("click", function(){
-            window.open(data.results[0].url, 'newAmazonPage');
+        amazonButton.addEventListener("click", function() {    // if click 'Buy on Amazon'...
+            window.open(data.results[0].url, 'newAmazonPage'); // open in new page
         })
-        var urlSplit = data.results[0].url.split("/")
-        var productId = urlSplit[5]
-        fetch("https://wolf-amazon-data-scraper.p.rapidapi.com/products/" + productId + "?api_key=59ef84be287bba26357f5519b0058332", {
+
+        var urlSplit = data.results[0].url.split("/"); // Split Amazon URL
+        var productId = urlSplit[5];                   // Store Amazon Product ID
+
+        fetch("https://wolf-amazon-data-scraper.p.rapidapi.com/products/" + productId + "?api_key=59ef84be287bba26357f5519b0058332", { // Call Amazon API w/Product ID
 	    "method": "GET",
 	    "headers": {
 		"x-rapidapi-host": "wolf-amazon-data-scraper.p.rapidapi.com",
@@ -52,30 +61,32 @@ function getAmazon(event){
 	    }
         })
         .then(function(response){
-            if (response.status === 200) { // after content loads...
+            if (response.status === 200) {        // after content loads...
                 card[0].style.display = "inline"; // Show card
-                spinner.style.display = "none"; // Hide spinner
+                spinner.style.display = "none";   // Hide spinner
                 return response.json();
-            } else if (response.status >= 500) {
-                Swal.fire('Error Try Again');
-                spinner.style.display = "none";
+            } else if (response.status >= 500) {  // if error...
+                Swal.fire('Error Try Again');     // Display error message
+                spinner.style.display = "none";   // Hide spinner
                 return;
             }
             
         }).then(function(data) {
-            amazonDescription.textContent = data.full_description;
-            amazonItemName.textContent = data.name;
+            amazonDescription.textContent = data.full_description; // Display Amazon Description
+            amazonItemName.textContent = data.name;                // Display Amazon Title
         })
     })
 }
 
-// Call eBay API
+// eBay API Function
 function getEbay(event){
-    event.preventDefault(); // Prevent Default
-    card[1].style.display = "none"; // Hide card
+    event.preventDefault();           // Prevent Default
+    card[1].style.display = "none";   // Hide card
     spinner.style.display = "inline"; // Show spinner
-    var userInput = document.querySelector("#userInput").value
-    fetch("https://ebay-products-search-scraper.p.rapidapi.com/products?query=" + userInput + "&page=1&Item_Location=north_america", {
+
+    var userInput = document.querySelector("#userInput").value; // Get User Input
+
+    fetch("https://ebay-products-search-scraper.p.rapidapi.com/products?query=" + userInput + "&page=1&Item_Location=north_america", { // Call eBay API
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "ebay-products-search-scraper.p.rapidapi.com",
@@ -83,38 +94,39 @@ function getEbay(event){
 	}
     })
     .then(function(response) {
-        if (response.status === 200) { // after content loads...
+        if (response.status === 200) {        // after content loads...
             card[1].style.display = "inline"; // Show card
             return response.json()
-        } else if (response.status >= 500){
-            Swal.fire('Error Try Again')
-            spinner.style.display = "none"
+        } else if (response.status >= 500) {  // if error...
+            Swal.fire('Error Try Again')      // Display error message
+            spinner.style.display = "none"    // Hide spinner
             return
         }  
     })
     .then(function(data){
-        shortRating = data.products[0].rating.split("")
-        ebayImg.src = data.products[0].image;
-        ebayPrice.textContent = data.products[0].price;
-        if (shortRating[0] === undefined) {
-            ebayRating.textContent = "No reviews yet ⭐"
-        } else {
-            ebayRating.textContent = shortRating[0] + " ⭐";
+        ebayImg.src = data.products[0].image;              // eBay Image
+        ebayPrice.textContent = data.products[0].price;    // eBay Price
+        ebayItemName.textContent = data.products[0].title; // eBay Title
+        shortRating = data.products[0].rating.split('');   // eBay Rating
+
+        if (shortRating[0] === undefined) {                  // if no reviews...
+            ebayRating.textContent = "No reviews yet ⭐";    // print 'no reviews'
+        } else {                                             // otherwise...
+            ebayRating.textContent = shortRating[0] + " ⭐"; // display reviews
         }
-        ebayItemName.textContent = data.products[0].title;
-        ebayButton.addEventListener("click", function(){
-            window.open(data.products[0].productLink, "newPage");
+
+        ebayButton.addEventListener("click", function () {        // if click 'Buy on eBay'...
+            window.open(data.products[0].productLink, "newPage"); // open in new page
         })
     })
 }
 
 // When user clicks 'Search'
-searchBtn.addEventListener("click", getEbay)
-searchBtn.addEventListener("click", getAmazon)
-searchBtn.addEventListener("keypress", function(event){
+searchBtn.addEventListener("click", getEbay)   // Call eBay API Function
+searchBtn.addEventListener("click", getAmazon) // Call Amazon API Function
+searchBtn.addEventListener("keypress", function (event) { // Press Enter to 🔎
     if (event.key === "Enter") {
         getEbay();
         getAmazon();
     }
 })
-
