571de9e8ae39ec3046d8990e43e47e09c324c582
Yaakov Yisrael Benjamin <yyisrael613@gmail.com>
2022-02-03 12:54:51 -0500

    added spinner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,28 @@
-var searchBtn = document.querySelector("#searchBtn")
-var amazonImg = document.querySelector("#amazonCardImage")
-var amazonDescription = document.querySelector("#amazonCardText")
-var amazonItemName = document.querySelector("#amazonItemName")
-var amazonPrice = document.querySelector("#amazonPrice")
-var amazonRating = document.querySelector("#amazonRating")
-var amazonButton = document.querySelector("#goToAmazon")
-var ebayImg = document.querySelector("#ebayImage")
-var ebayItemName = document.querySelector("#ebayItemName")
-var ebayPrice = document.querySelector("#ebayPrice")
-var ebayRating = document.querySelector("#ebayRating")
-var ebayButton = document.querySelector("#goToEbay")
+// Initialize Variables
+var searchBtn = document.querySelector("#searchBtn");
+var card = document.getElementsByClassName("card");
+var amazonImg = document.querySelector("#amazonCardImage");
+var amazonDescription = document.querySelector("#amazonCardText");
+var amazonItemName = document.querySelector("#amazonItemName");
+var amazonPrice = document.querySelector("#amazonPrice");
+var amazonRating = document.querySelector("#amazonRating");
+var amazonButton = document.querySelector("#goToAmazon");
+var ebayImg = document.querySelector("#ebayImage");
+var ebayItemName = document.querySelector("#ebayItemName");
+var ebayPrice = document.querySelector("#ebayPrice");
+var ebayRating = document.querySelector("#ebayRating");
+var ebayButton = document.querySelector("#goToEbay");
+var spinner = document.querySelector(".spinner-border");
 
-
-
+// Call Amazon API
 function getAmazon(event){
+    event.preventDefault() // Prevent Default
+
+    card[0].style.display = "none"; // Hide card
+    spinner.style.display = "inline"; // Show spinner
+
     var userInput = document.querySelector("#userInput").value
     // console.log(userInput)
-    event.preventDefault()
     // console.log("you clicked the search button")
     var searchURL = "https://wolf-amazon-data-scraper.p.rapidapi.com/search/"+ userInput +"?api_key=59ef84be287bba26357f5519b0058332"
     fetch(searchURL, {
@@ -29,6 +35,10 @@ function getAmazon(event){
     .then(function(response){
         // console.log('you hit the response section');
         // console.log(response.json())
+        if (response.status === 200) { // after content loads...
+            card[0].style.display = "inline"; // Show card
+            spinner.style.display = "hide"; // Hide spinner
+        }
         return response.json();
     })
     .then(function(data) {
@@ -71,13 +81,15 @@ function getAmazon(event){
             amazonItemName.textContent = data.name;
         })
     })
-    
-
-    
 }
 
+// Call eBay API
 function getEbay(event){
-    event.preventDefault();
+    event.preventDefault(); // Prevent Default
+
+    card[1].style.display = "none"; // Hide card
+    spinner.style.display = "inline"; // Show spinner
+
     var userInput = document.querySelector("#userInput").value
     // console.log("running getEbay Function")
     fetch("https://ebay-products-search-scraper.p.rapidapi.com/products?query=" + userInput + "&page=1&Item_Location=north_america", {
@@ -88,6 +100,10 @@ function getEbay(event){
 	}
     })
     .then(function(response) {
+        if (response.status === 200) { // after content loads...
+            card[1].style.display = "inline"; // Show card
+            spinner.style.display = "hide"; // Hide spinner
+        }
         return response.json()
     })
     .then(function(data){
@@ -111,9 +127,9 @@ function getEbay(event){
             window.open(data.products[0].productLink, "newPage");
         })
     })
-
 }
 
+// When user clicks 'Search'
 searchBtn.addEventListener("click", getEbay)
 searchBtn.addEventListener("click", getAmazon)
 searchBtn.addEventListener("keypress", function(event){
