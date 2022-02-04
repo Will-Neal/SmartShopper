225597c08dc882357ba7a69f11c99072cc0f9d6b
Yaakov Yisrael Benjamin <yyisrael613@gmail.com>
2022-02-04 00:31:58 -0500

    added save search history functionality via local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // Initialize Variables
 // GENERAL
 var searchBtn = document.querySelector("#searchBtn"); // Search Button
+var historyEl = document.querySelector("#history");   // Search History
 var card = document.getElementsByClassName("card");   // Info Card
 var spinner = document.querySelector(".spinner-border"); // Spinner
 
@@ -21,7 +22,9 @@ var ebayButton = document.querySelector("#goToEbay"); // Buy on eBay
 
 // Amazon API Function
 function getAmazon(event){
-    event.preventDefault()            // Prevent Default
+    if (event != undefined) {
+        event.preventDefault()        // Prevent Default
+    }
     card[0].style.display = "none";   // Hide card
     spinner.style.display = "inline"; // Show spinner
 
@@ -83,7 +86,9 @@ function getAmazon(event){
 
 // eBay API Function
 function getEbay(event){
-    event.preventDefault();           // Prevent Default
+    if (event != undefined) {
+        event.preventDefault();       // Prevent Default
+    }
     card[1].style.display = "none";   // Hide card
     spinner.style.display = "inline"; // Show spinner
 
@@ -99,11 +104,11 @@ function getEbay(event){
     .then(function(response) {
         if (response.status === 200) {        // after content loads...
             card[1].style.display = "inline"; // Show card
-            return response.json()
+            return response.json();
         } else if (response.status >= 500) {  // if error...
-            Swal.fire('Error Try Again')      // Display error message
-            spinner.style.display = "none"    // Hide spinner
-            return
+            Swal.fire('Error Try Again');     // Display error message
+            spinner.style.display = "none";   // Hide spinner
+            return;
         }  
     })
     .then(function(data){
@@ -125,11 +130,55 @@ function getEbay(event){
 }
 
 // When user clicks 'Search'
-searchBtn.addEventListener("click", getEbay)   // Call eBay API Function
-searchBtn.addEventListener("click", getAmazon) // Call Amazon API Function
-searchBtn.addEventListener("keypress", function (event) { // Press Enter to 🔎
+searchBtn.addEventListener("click", getEbay)      // Call eBay API Function
+searchBtn.addEventListener("click", getAmazon)    // Call Amazon API Function
+searchBtn.addEventListener("click", appendSearch) // Append to Search History
+
+// Press Enter to 🔎
+searchBtn.addEventListener("keypress", function (event) { 
     if (event.key === "Enter") {
-        getEbay();
-        getAmazon();
+        getEbay();      // Call eBay API Function
+        getAmazon();    // Call Amazon API Function
+        appendSearch(); // Append to Search History
     }
 })
+
+// GET SEARCH HISTORY FROM LOCAL STORAGE (ON PAGE LOAD)
+var searchesArray = JSON.parse(localStorage.getItem("searches")) || []; // Get from Local Storage
+appendSearch();                                                         // Display History
+
+// Append Search History Function
+function appendSearch() {
+    // SAVE SEARCH HISTORY TO LOCAL STORAGE
+    var userInput = document.querySelector("#userInput").value;  // User input variable
+    
+    if (!searchesArray.includes(userInput) && userInput != '') { // if input is not in history...
+        searchesArray.unshift(userInput);                        // push entered city to array
+    }
+
+    if (searchesArray.length > 3) {                        // if array length > 3...
+        searchesArray.splice(searchesArray.length - 1, 1); // remove last element
+    }
+
+    var searchesString = JSON.stringify(searchesArray);    // convert array -> string
+    localStorage.setItem("searches", searchesString);      // push to Local Storage
+
+    // DISPLAY SEARCH HISTORY
+    historyEl.innerHTML = '';                              // reset history
+
+    for (var i = 0; i < searchesArray.length; i++) {
+        var historyBtn = document.createElement("button"); // Create button
+        historyBtn.textContent = searchesArray[i];         // Inject search query
+        historyBtn.setAttribute("class", "history-btn");   // Set class    
+        historyEl.append(historyBtn);                      // Append button to history            
+    }
+
+    document.querySelectorAll(".history-btn").forEach(function (ele) {
+        ele.addEventListener('click', function (event) {                  // if click on history ele...
+            event.preventDefault();                                       // Prevent Default
+            document.querySelector("#userInput").value = ele.textContent; // Update input field
+            getEbay();   // Call eBay API Function
+            getAmazon(); // Call Amazon API Function
+        });
+    });
+}
